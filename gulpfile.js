7cf0206a512848b50a302e1f13392ae536d8ae8e
Adrian Hernandez Barrios <adrian2monk@gmail.com>
2016-11-15 01:47:50 -0500

    Setup gulpfile with babel and sass. Karma test are initialized

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,62 @@
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var streamify = require('gulp-streamify');
-var uglify = require('gulp-uglify');
-var htmlreplace = require('gulp-html-replace');
-var jshint = require('gulp-jshint');
-var mocha = require('gulp-mocha');
-var chai = require('chai');
-var request = require('supertest');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = new gulpLoadPlugins();
 
+var Server = plugins.karma.Server;
 var path = {
-    html: 'client/index.html',
-    server: ['server/*.js', 'server/**/*.js'],
+    html: 'demo.html',
     test: ['test/*.spec.js'],
-    client: 'client/src/app.js',
-    min_client: 'client.min.js',
-    out_client: 'client.js',
-    dest: 'build'
+    source: 'src/*.js',
+    styles: 'src/*.scss',
+    min: 'ng-duration-picker.min.js',
+    dest: 'dist'
 };
 
 // test tasks
-gulp.task('test', function() {
-    gulp.src(path.test)
-        .pipe(mocha({
-      reporter: 'spec',
-      globals: {
-    expect: chai.expect,
-    assert: chai.assert,
-    request: request
-      }
-  }));
-    chai.should();
+gulp.task('test', function(done) {
+  return new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
 // server tasks
 gulp.task('lint', function() {
-    gulp.src(path.server)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'));
+  return gulp.src(path.source)
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
-gulp.task('serve', function() {
-    nodemon({
-  script: 'server/app.js',
-  ext: 'js',
-  ignore: ['client/*'],
-  tasks: ['watch', 'lint'],
-  env: { 'NODE_ENV': 'development' }
-    });
+gulp.task('serve', ['lint', 'styles'], function() {
+  return gulp.src('.')
+    .pipe(plugins.webserver({
+      fallback: path.html,
+      livereload: true,
+    })); 
 });
 
 // development tasks
-gulp.task('copy', function() {
-    gulp.src(path.html)
-        .pipe(gulp.dest(path.dest));
-});
-gulp.task('watch', function() {
-    gulp.watch(gulp.src(path.html, ['copy']));
-    var watcher = watchify(browserify({
-  entries: [path.client],
-  transform: [reactify],
-  debug: true,
-  cache: { },
-  packageCache: { },
-  fullPaths: true
-    }));
-    return watcher.on('update', function() {
-  watcher.bundle()
-      .pipe(source(path.out_client))
-      .pipe(gulp.dest(path.dest));
-  console.log('Updated');
-    })
-  .bundle()
-  .pipe(source(path.dest))
-  .pipe(gulp.dest(path.out_client));
+gulp.task('styles', function () {
+  return gulp.src(path.styles)
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(path.dest))
 });
 gulp.task('default', ['serve']);
 
 // production tasks
-gulp.task('build', function() {
-    browserify({
-  entries: [path.client],
-  transform: [reactify]
-    })
-  .bundle()
-  .pipe(source(path.min_client))
-  .pipe(streamify(uglify(path.min_client)))
-  .pipe(gulp.dest(path.dest));
+gulp.task('build', ['lint', 'styles'], function() {
+  return gulp.src(path.source)
+    .pipe(plugins.plumber())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.babel({
+        presets: ['es2015']
+    }))
+    .pipe(plugins.concat(path.min))
+    .pipe(plugins.ngAnnotate())
+    .pipe(plugins.uglify())
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(path.dest));
 });
-gulp.task('replace', function() {
-    gulp.src(path.html)
-        .pipe(htmlreplace({
-      'js': 'build/' + path.min_client
-  }))
-        .pipe(gulp.dest(path.dest));
-});
-gulp.task('production', ['replace', 'build']);
 
 
